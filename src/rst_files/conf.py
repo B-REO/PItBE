682b0686441cea33412f6aafbb9e12df2af6c9a0
Baba Reo <moon@qclnoMacBook-Pro.local>
2024-06-13 05:32:39 +0900

    17th version

--- src/rst_files/conf.py
+++ src/rst_files/conf.py
@@ -60,8 +60,29 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# HTML出力用の設定
+html_theme_options = {
+    'style_external_links': True,
+    'style_nav_header_background': '#2980B9',
+    'style_text': '#2C3E50',
+    'style_text_selection': '#E74C3C',
+    'style_bg': '#ECF0F1',
+    'style_bg_selection': '#BDC3C7',
+    'style_link': '#3498DB',
+    'style_link_hover': '#2980B9',
+    'style_code': '#3498DB',
+    'style_code_font_size': '0.85em',
+    'style_pre': '#1ABC9C',
+    'style_pre_font_size': '0.85em',
+    'style_border': '#BDC3C7',
+    'style_outlink': '#1ABC9C',
+}
+# スクロールバーのスタイルを設定
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
 
 # -- Options for todo extension ----------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
