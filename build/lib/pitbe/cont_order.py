0d4ce53320d5130125da653d1092952025b60e93
Baba Reo <moon@qclnoMacBook-Pro.local>
2025-06-25 14:45:38 +0900

    bye-bye build

--- build/lib/pitbe/cont_order.py
+++
@@ -1,6 +0,0 @@
-def cont_order(order, rank):
-    by_order = format(order, ('0' + str(rank) + 'b'))
-    cont_list = []
-    for i in range(len(by_order)):
-        cont_list.append(int(by_order[i]))
-    return cont_list
